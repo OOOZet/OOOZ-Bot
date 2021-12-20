39dd76884cf5a31fde95ce19961f5114493cfc7e
InfoTCube <infocube.yout@gmail.com>
2021-12-20 12:19:55 +0100

    some fun commands added

--- src/events/Fun.js
+++ src/events/Fun.js
@@ -70,12 +70,14 @@ module.exports = (msg, cmd, ...args) => {
         case "ping":
             msg.reply("Pong! `" + (Math.abs(Date.now() - msg.createdTimestamp)) + "ms`");
             break
-        case "guess-my-age":
+        case "guess-me":
             try {
                 (async () => {
-                    if (args[0].match(/^[a-zA-Z]+$/)) {
-                        const data = await (await axios.get(`https://api.agify.io/?name=${args[0]}`)).data;
-                        msg.reply(`I think you are ${data.age} years old`)
+                    if (args.length > 0 && args[0].match(/^[a-zA-Z]+$/)) {
+                        const age = await (await axios.get(`https://api.agify.io/?name=${args[0]}`)).data;
+                        const gender = await (await axios.get(`https://api.genderize.io?name=${args[0]}`)).data;
+                        const nation = await (await axios.get(`https://api.nationalize.io?name=${args[0]}`)).data;
+                        msg.reply(`I think you are ${age.age} years old ${gender.gender} from ${nation.country[0].country_id}`)
                     } else {
                         msg.reply('Invalid characters were used');
                     }      
@@ -100,15 +102,66 @@ module.exports = (msg, cmd, ...args) => {
                 msg.reply('something went wrong!');
             }
             break
-        case "chart": 
+        case "qr":
             try {
                 (async () => {
-                    const data = await (await axios.get(`https://quickchart.io/chart?bkg=white&c=%7B%0A%20%20%22type%22%3A%20%22outlabeledPie%22%2C%0A%20%20%22data%22%3A%20%7B%0A%20%20%20%20%22labels%22%3A%20%5B%22for%22%2C%20%22against%22%2C%20%22indifferent%22%5D%2C%0A%20%20%20%20%22datasets%22%3A%20%5B%7B%0A%20%20%20%20%20%20%20%20%22backgroundColor%22%3A%20%5B%22%2313a813%22%2C%20%22%23a61212%22%2C%20%22%23dde009%22%5D%2C%0A%20%20%20%20%20%20%20%20%22data%22%3A%20%5B${args[0]}%2C%20${args[1]}%2C%20${args[2]}%5D%0A%20%20%20%20%7D%5D%0A%20%20%7D%2C%0A%20%20%22options%22%3A%20%7B%0A%20%20%20%20%22plugins%22%3A%20%7B%0A%20%20%20%20%20%20%22legend%22%3A%20false%2C%0A%20%20%20%20%20%20%22outlabels%22%3A%20%7B%0A%20%20%20%20%20%20%20%20%22text%22%3A%20%22%25l%20%25p%22%2C%0A%20%20%20%20%20%20%20%20%22color%22%3A%20%22white%22%2C%0A%20%20%20%20%20%20%20%20%22stretch%22%3A%2035%2C%0A%20%20%20%20%20%20%20%20%22font%22%3A%20%7B%0A%20%20%20%20%20%20%20%20%20%20%22resizable%22%3A%20true%2C%0A%20%20%20%20%20%20%20%20%20%20%22minSize%22%3A%2012%2C%0A%20%20%20%20%20%20%20%20%20%20%22maxSize%22%3A%2018%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D`)).data;
                     const embed = new MessageEmbed()
                         .setColor('RANDOM')
-                        .setTitle('Voting results')
-                        .setImage(`https://quickchart.io/chart?bkg=white&c=%7B%0A%20%20%22type%22%3A%20%22outlabeledPie%22%2C%0A%20%20%22data%22%3A%20%7B%0A%20%20%20%20%22labels%22%3A%20%5B%22for%22%2C%20%22against%22%2C%20%22abstain%22%5D%2C%0A%20%20%20%20%22datasets%22%3A%20%5B%7B%0A%20%20%20%20%20%20%20%20%22backgroundColor%22%3A%20%5B%22%2313a813%22%2C%20%22%23a61212%22%2C%20%22%23dde009%22%5D%2C%0A%20%20%20%20%20%20%20%20%22data%22%3A%20%5B${args[0]}%2C%20${args[1]}%2C%20${args[2]}%5D%0A%20%20%20%20%7D%5D%0A%20%20%7D%2C%0A%20%20%22options%22%3A%20%7B%0A%20%20%20%20%22plugins%22%3A%20%7B%0A%20%20%20%20%20%20%22legend%22%3A%20false%2C%0A%20%20%20%20%20%20%22outlabels%22%3A%20%7B%0A%20%20%20%20%20%20%20%20%22text%22%3A%20%22%25l%20%25p%22%2C%0A%20%20%20%20%20%20%20%20%22color%22%3A%20%22white%22%2C%0A%20%20%20%20%20%20%20%20%22stretch%22%3A%2035%2C%0A%20%20%20%20%20%20%20%20%22font%22%3A%20%7B%0A%20%20%20%20%20%20%20%20%20%20%22resizable%22%3A%20true%2C%0A%20%20%20%20%20%20%20%20%20%20%22minSize%22%3A%2012%2C%0A%20%20%20%20%20%20%20%20%20%20%22maxSize%22%3A%2018%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D`)
-                        .setFooter(`data from quickchart.io`)
+                        .setTitle(args[0].toString())
+                        .setImage(`https://api.qrserver.com/v1/create-qr-code/?data=${args[0]}`)
+                        .setFooter(`data from goqr.me`)
+                        .setTimestamp();
+                    msg.reply({ embeds: [embed] })
+                })();
+            } catch (e) {
+                msg.reply('something went wrong!');
+            }
+            break
+        case "motivation":
+            try {
+                (async () => {
+                    const data = await (await axios.get(`https://www.affirmations.dev`)).data;
+                    const embed = new MessageEmbed()
+                        .setColor('RANDOM')
+                        .setTitle('Motivation')
+                        .setDescription(data.affirmation.toString())
+                        .setFooter(`data from affirmations.dev`)
+                        .setTimestamp();
+                    msg.reply({ embeds: [embed] })
+                })();
+            } catch (e) {
+                msg.reply('something went wrong!');
+            }
+            break
+        case "crypto":
+            try {
+                (async () => {
+                    let crypto = 'btc'
+                    if(args.length > 0)
+                        crypto = args[0]
+
+                        const data = await (await axios.get(`https://api.cryptonator.com/api/ticker/${crypto}-usd`)).data;
+                    const embed = new MessageEmbed()
+                        .setColor('RANDOM')
+                        .setDescription(parseFloat(data.ticker.price, 10).toFixed(2).toString()+"$")
+                        .setTitle(crypto)
+                        .setFooter(`data from api.cryptonator.com`)
+                        .setTimestamp();
+                    msg.reply({ embeds: [embed] })
+                })();
+            } catch (e) {
+                msg.reply('something went wrong!');
+            }
+            break
+        case "fact":
+            try {
+                (async () => {
+                    const data = await (await axios.get(`https://uselessfacts.jsph.pl/random.json?language=en`)).data;
+                    const embed = new MessageEmbed()
+                        .setColor('RANDOM')
+                        .setDescription(data.text.toString())
+                        .setTitle('Fact')
+                        .setFooter(`data from uselessfacts.jsph.pl`)
                         .setTimestamp();
                     msg.reply({ embeds: [embed] })
                 })();
